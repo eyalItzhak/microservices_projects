3c8893b926152407c81001b45b68963883151816
EyalQ-LAB <Eyalpross101@gmail.com>
2023-12-31 21:02:10 +0200

    demo end

--- ticketing/nats-test/src/listener.ts
+++ ticketing/nats-test/src/listener.ts
@@ -1,6 +1,8 @@
-import nats from "node-nats-streaming";
+import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
+
 console.clear();
-const stan = nats.connect("ticketing", "123", {
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   //we use kubectl port-forward nats-depl-75b9dff744-m4kr8 4222:4222
   url: "http://localhost:4222",
 });
@@ -8,9 +10,37 @@ const stan = nats.connect("ticketing", "123", {
 stan.on("connect", () => {
   console.log("listener connect to NATS");
 
-  const subscription = stan.subscribe("ticket:created");
+  stan.on("close", () => {
+    console.log("NATS connection closed!");
+    process.exit();
+  });
 
-  subscription.on("message", (msg) => {
-    console.log("Message received");
+  const option = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName("accounting-server");
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "queue-group-name",
+    option
+  );
+
+  subscription.on("message", (msg: Message) => {
+    const data = msg.getData();
+    if (typeof data === "string") {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+    msg.ack();
   });
 });
+
+process.on("SIGINT", () => {
+  stan.close();
+});
+process.on("SIGTERM", () => {
+  // not working on windows
+  stan.close();
+});
+
+//http://localhost:8222/streaming/channelsz?subs=1
