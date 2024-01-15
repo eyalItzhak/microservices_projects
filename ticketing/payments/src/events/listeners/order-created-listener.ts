08c423ac1235e97046e74b369adc3f435f2295c1
Eyal <eyalpross101@gmail.com>
2024-01-15 17:57:55 +0200

    create order listern

---
+++ ticketing/payments/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,26 @@
+import {
+  Listener,
+  OrderCreatedEvent,
+  OrderStatus,
+  Subjects,
+} from "@eyaltickets/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+import { Order } from "../../models/orders";
+
+export class orderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const order = Order.build({
+      id: data.id,
+      price: data.ticket.price,
+      status: data.status,
+      userId: data.userId,
+      version: data.version,
+    });
+
+    order.save();
+    msg.ack();
+  }
+}
